8d32f652487a2d228bd1ba1c28b213e9f85505e7
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-01-26 08:25:09 +0800

    made a new calculator design

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -12,27 +12,32 @@ const Table = ({handleClick}: Props) => {
             <table>
                 <tbody>
                     <tr>
-                        <td colSpan={2} onClick={e => handleClick('clear')}>clear</td>
-                        <td onClick={e => handleClick('=')}>=</td>
-                        <td onClick={e => handleClick('+')}>+</td>
+                        <td className="saddlebrown" colSpan={2} onClick={e => handleClick('clear')}>clear</td>
+                        <td className="blue" onClick={e => handleClick('%')}>%</td>
+                        <td className="blue" onClick={e => handleClick('+')}>+</td>
                     </tr>
                     <tr>
                         <td onClick={e => handleClick(7)}>7</td>
                         <td onClick={e => handleClick(8)}>8</td>
                         <td onClick={e => handleClick(9)}>9</td>
-                        <td onClick={e => handleClick('/')}>/</td>
+                        <td className="blue" onClick={e => handleClick('/')}>/</td>
                     </tr>
                     <tr>
                         <td onClick={e => handleClick(4)}>4</td>
                         <td onClick={e => handleClick(5)}>5</td>
                         <td onClick={e => handleClick(6)}>6</td>
-                        <td onClick={e => handleClick('x')}>x</td>
+                        <td className="blue" onClick={e => handleClick('x')}>x</td>
                     </tr>
                     <tr>
                         <td onClick={e => handleClick(1)}>1</td>
                         <td onClick={e => handleClick(2)}>2</td>
                         <td onClick={e => handleClick(3)}>3</td>
-                        <td onClick={e => handleClick('-')}>-</td>
+                        <td className="blue" onClick={e => handleClick('-')}>-</td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2} onClick={e => handleClick(0)}>0</td>
+                        <td onClick={e => handleClick('.')}>.</td>
+                        <td className="blue" onClick={e => handleClick('=')}>=</td>
                     </tr>
                 </tbody>
             </table>
